da4fb3387ad7d7b112e2fe73efb08fe8723d390b
e d u a r d a <86473064+eduardarn@users.noreply.github.com>
2021-12-09 13:15:32 -0300

    Add files via upload

---
+++ contain/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace contain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "vamos testar o codigo";
+            string ndois = "testar";
+            string ntres = "codigo";
+
+            Console.WriteLine("A frase não estará correta, pois falta a palavra testar :{0} ", num.Contains(ndois));
+
+            Console.WriteLine("A frase estará correta, pois há a palavra codigo :{0} ", num.Contains(ntres));
+        }
+    }
+}
